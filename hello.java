16ba9ffd3870939f7ea673e5d08e9f0c5ea58d9a
sharathbabusrinivas <32052154+sharathbabusrinivas@users.noreply.github.com>
2017-09-27 09:49:20 -0400

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,32 @@
+
+
+
+//life motto
+if(sad()==true){
+    sad().stop();
+    beawesome();
+}
+
+
+
+
+
+
+
+
+
+
+
+System.out.println("I am an ambivert")
+if(boredtoday)
+gotravel();
+else
+gotravel();
+switch(interests){
+    case freetime:readquora();
+                  break;
+    default: while(true)
+	         sleep();
+}
+
+
